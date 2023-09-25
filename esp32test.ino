9d12fdfa10ced734cd11bec1ff702a270ff09124
AbdulWahabAwan <alvilahori@hotmail.com>
2023-09-25 19:52:19 +0500

    First Upload

---
+++ esp32test.ino
@@ -0,0 +1,36 @@
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+
+
+void setup() {
+    // WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+    // it is a good practice to make sure your code sets wifi mode how you want it.
+
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+
+
+    wm.resetSettings();
+
+
+    bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+    res = wm.autoConnect("ElexitySM","Admin"); // password protected ap
+
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected...yeey :)");
+    }
+
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:   
+}
